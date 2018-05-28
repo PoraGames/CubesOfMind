6ce165b4d4eb0f0c67b742d1c7c61bb5b5a94eca
ProRak <ProRak25@yandex.ru>
2018-05-28 21:45:45 +0500

    ++ Куб

---
+++ CubesOfMind/Assets/Scripts/Cube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public Vector3Int needPosition;
+
+    void Awake()
+    {
+        needPosition = new Vector3Int((int)transform.position.x, (int)transform.position.y, (int)transform.position.z);
+    }
+
+    void Update()
+    {
+        transform.position = needPosition; // Временно
+    }
+}
